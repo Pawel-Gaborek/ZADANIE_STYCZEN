ea7d4b4d0c288bd25d8fe4f2d490766c0ac4634f
Pawel-Gaborek <44684528+Pawel-Gaborek@users.noreply.github.com>
2019-02-13 21:42:54 +0100

    Aktualizacja kodu nr 2

--- GABOREK_STYCZEN_NR-2.c
+++ GABOREK_STYCZEN_NR-2.c
@@ -70,35 +70,127 @@
 //zupełnej ostateczności. Często gotowce są bardziej „wizją” autora, która nijak ma się do sedna problemu.
 //Oddanie gotowca będzie się równało z oceną niedostateczną na koniec semestru.
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//************************************************************************************************************
+//
+//========================================   WAŻNE   !!!!!!!!    =============================================
+//
+//   w poniższym zadniu opierałem się na podstawie wykładów Pana dr hab. Wojciech Bożejko z Politechniki
+//   Wrocławskiej (Pan dr Fedorow nie umieścił materiałów na e-dziekanacie a z książki "Programowanie w C",
+//   autor Ritchie Kernighan, jeszcze nie przerobiłem do końca)
+//
+//
+//************************************************************************************************************
+
+
+
 typedef struct Node Node;
 
-struct Node;
+struct Node
+{
+    int Value;
+    Node * Next;
+};
 
-int Value;
-Node * Next;
+Node * Create (int value)
+{
+    
+    Node * zmiennaPoczatkowa=NULL;
+    
+    Node * zmiennaTymczasowa;
+    zmiennaTymczasowa = (Node*)malloc(sizeof(Node));
+    
+    zmiennaTymczasowa->Value = value;
+    zmiennaTymczasowa->Next = zmiennaPoczatkowa;
+    zmiennaPoczatkowa = zmiennaTymczasowa;
+    
+    return 0;
+}
 
-Node * Create (int value);          // tworzy listę jednoelementową (alokuje węzeł), która ma węzeł wskazujący
-                                    //na samego siebie jako następnik
 
-Node* GetTail(Node* root);          //metoda musi ustalić pozycję ostatniego elementu mając dostęp do korzenia
-                                    //i zwrócić jego adres (uwaga - nie jest tu wymagany malloc!)
+Node * GetTail(Node * root);
 
-void Add(Node* root, int value);    //dodaje węzeł dolisty, której KORZEŃ jest przekazany jako parametr, do
-                                    //znalezienia końca listy należy użyć metody GetTail() z powyżej
+{
+    Node * zmiennaTymczasowa;
+    while (root != NULL)
+    {
+        zmiennaTymczasowa = root;
+        zmiennaTymczasowa = zmiennaTymczasowa->Next)
+    }
+    return zmiennaTymczasowa;
+}
+    
+    
+void Add(Node* root, int value);
 
-void PrintList(Node* root);         //metoda powinna wypisać listę, mając tylko jej korzeń. Należy użyć
-                                    //GetTail() do
-                                    //określenia, kiedy zakończyć wypisywanie
+{
+    *elementKoncowy = GetTail(root);
+    
+    Node *zmiennaPomocnicza;
+    zmiennaPomocnicza = (Node*)malloc(sizeof(Node));
+      
+    zmiennaPomocnicza->Value = value;
+    zmiennaPomocnicza->Next = elementKoncowy->Next;
+    elementKoncowy->Next = zmiennaPomocnicza;
+    return 0;
+}
+    
+void PrintList(Node * root);
 
-void RemoveNode(Node * root, int value);    // metoda powinna usunąć węzeł o WARTOŚCI podanej jako parametr,
-                                            //mając tylko korzeń listy. UWAGA - należy usunąć węzeł, zwolnić
-                                            //pamięć po nim inie naruszyć integralności listy (powinna
-                                            //nastąpić zmiana następnika węzła PRZED usuwanym węzłem, na
-                                            //zasadzie: 2->4->8->16->2->(...), usuwamy węzeł 4, nowa lista: 2
-                                            //>8->16->2->(...)
-void FreeList(Node * root);         //metoda czyści pamięć po liście, usuwając wszystkie węzły metodą free()
-                                    //int Count(Node *root); // metoda zwraca rozmiar listy
+{
+    
+    
+    *adresOstatniegoElementu = GetTail(root);
+    
+    for(zmiennaTymczasowa = root ; zmiennaTtymczasowa == adresOstatniogoElementu ; zmiennaTymczasowa = zmiennaTymczasowa->Next)
+    {
+        printf ("Twoim elementem listy o adresie:", zmiennaTymczasowa);
+        printf ("Jest element o wartosci: %i",
+                zmiennaTymczasowa->Value);
+    }
+}
 
+void RemoveNode(Node * root, int value);
+
+{
+    Node *zmiennaPomocnicza;
+    
+    zmiennaTymczasowa = root;
+    
+    
+    while (zmiennaTymczasowa != NULL)
+        
+        zmiennaTymczasowa = zmiennaTymczasowa->Next)
+        
+        if (zmiennaPomocnicza->Value = value)
+        {
+            zmiennaPomocnicza = root;
+            root = zmiennaPomocnicza->Next;
+            delete zmiennaPomocnicza;
+        }
+        else
+        
+            zmiennaTymczasowa = zmiennaTymczasowa->Next)
+
+}
+
+void FreeList(Node * root);
+
+    {
+        Node *zmiennaPomocnicza;
+        while (root != NULL)
+            {
+                zmiennaPomocnicza = root;
+                root = zmiennaPomocnicza->Next;
+                free(zmiennaPomocnicza);
+            }
+    }
+    
+    
 
 int main(void)
 {
@@ -121,24 +213,36 @@ int main(void)
         Add (root, 4);
         Add (root, 8);
         Add (root, 16);
+        
         PrintList (root); // 2 -> 4 -> 8 -> 16
         
         Add (root, 20);
+        
         PrintList (root); // 2 -> 4 -> 8 -> 16 -> 20
-        printf ("Ostatni wezel ma wartosc %d", GetTail(root)->Value); // 20 Add(root, 16);
-        PrintList(root); // 2 -> 4 -> 8 -> 16 -> 20 -> 16 printf("%d", Count(root)); // 6
+        
+        printf ("Ostatni wezel ma wartosc %d", GetTail(root)->Value); // 20
+        
+        Add(root, 16);
+       
+        PrintList(root); // 2 -> 4 -> 8 -> 16 -> 20 -> 16
+        
+        printf("%d", Count(root)); // 6
         
         RemoveNode(root, 8);
-        PrintList(root); // 2 -> 4 -> 16 -> 20 -> 16 RemoveNodef(root, 10); // element nieistniejący PrintList(root); // 2 -> 4 -> 16 -> 20 -> 16
-        RemoveNodefroot, 16); // usuń wszystkie węzły o wartości 16 PrintList(root); // 2 -> 4 -> 20
+        
+        PrintList(root); // 2 -> 4 -> 16 -> 20 -> 16
+        
+        RemoveNodef(root, 10); // element nieistniejący
+        
+        PrintList(root); // 2 -> 4 -> 16 -> 20 -> 16
+        
+        RemoveNode (root, 16); // usuń wszystkie węzły o wartości 16 PrintList(root); // 2 -> 4 -> 20
         
         printf("%d", Count(root)); // 3
         
         FreeList(root); // zwolnienie pamięci na wszystkie węzły. PrintList(root); // (nic, lista niezaalokowana)
         return 0;
-        
     
-        Il TODO- implementacja funkcji z sygnatur
         
         puts ("\nSuper ! Wlasnie program nr 2 zakonczyl dzialanie");
         while(1)
